ff5ff1017cf0b68f5dfa0336164dae26eef0409a
shaslibansi <shaslibansi6@gmail.com>
2026-06-30 16:01:41 +0800

    add

--- src/Components.tsx
+++ src/Components.tsx
@@ -1,35 +1,9 @@
 import { useState } from 'react';
 
 function Components() {
-  const [car, setCar] = useState({
-    name: 'ford',
-    model: 'mustang',
-    year: 2024,
-  });
-
-  function handleAddCar() {}
-  function handleRemoveCar() {}
-  function handleNameChange() {}
-  function handleModelChange() {}
-  function handleYearChange() {}
   return (
     <div>
-      <p>Car</p>
-      <p>
-        {car.name}
-        {car.model}
-        {car.year}
-      </p>
-      <input type="text" value={car.name}></input>
-      <br />
-      <br />
-      <input type="text" value={car.model}></input>
-      <br />
-      <br />
-      <input type="number" value={car.year}></input>
-      <br />
-      <br />
-      <button>add car</button>
+      <p>car model</p>
     </div>
   );
 }
