2f0596441b9728083c40cc53bc89f6aaca5948ae
shaslibansi <shaslibansi6@gmail.com>
2026-06-30 17:33:34 +0800

    add

--- src/Components.tsx
+++ src/Components.tsx
@@ -1,31 +1,28 @@
 import { useState } from 'react';
 
 function Components() {
-  const [cars, setCar] = useState([]);
+  const [cars, setCars] = useState([]);
   const [carName, setCarName] = useState('');
   const [carModel, setCarModel] = useState('');
   const [carYear, setCarYear] = useState(new Date().getFullYear());
 
   function handleAddCar() {
-    const newCar = {
-      name: carName,
-      model: carModel,
-      year: carYear,
-    };
-    setCar((c) => [...c, newCar]);
-
+    const newCar = { name: carName, model: carModel, year: carYear };
+    setCars((cars) => [...cars, newCar]);
     setCarName('');
     setCarModel('');
     setCarYear(new Date().getFullYear());
   }
-  function handleRemoveCar() {}
+  function handleRemoveCar(index) {
+    setCars((c) => c.filter((car, i) => i !== index));
+  }
   function handleCarNameChange(event) {
     setCarName(event.target.value);
   }
-  function handleCarMakeChange(event) {
+  function handlecarModelChange(event) {
     setCarModel(event.target.value);
   }
-  function handleCarYearChange(event) {
+  function handlecarYearChange(event) {
     setCarYear(event.target.value);
   }
 
@@ -34,37 +31,35 @@ function Components() {
       <p>car model</p>
       <ul>
         {cars.map((car, index) => (
-          <li key={index}>
+          <li key={index} onClick={() => handleRemoveCar(index)}>
             {car.name}
             {car.model}
             {car.year}
           </li>
         ))}
       </ul>
-
       <input
         type="text"
-        value={carName}
-        placeholder="input car name"
         onChange={handleCarNameChange}
+        value={carName}
+        placeholder="enter car name"
       ></input>
       <br />
       <input
         type="text"
+        onChange={handlecarModelChange}
         value={carModel}
-        placeholder="input car model"
-        onChange={handleCarMakeChange}
+        placeholder="enter car model"
       ></input>
       <br />
       <input
         type="number"
+        onChange={handlecarYearChange}
         value={carYear}
-        placeholder="input car year"
-        onChange={handleCarYearChange}
       ></input>
       <br />
       <br />
-      <button onClick={handleAddCar}>Add Car</button>
+      <button onClick={handleAddCar}>add car</button>
     </div>
   );
 }
