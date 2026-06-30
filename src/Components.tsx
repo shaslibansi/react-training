cdd841c3a07e1e2edfa1697dd244ce81236e50b2
shaslibansi <shaslibansi6@gmail.com>
2026-06-30 15:43:18 +0800

    add

--- src/Components.tsx
+++ src/Components.tsx
@@ -1,9 +1,29 @@
 import { useState } from 'react';
 
 function Components() {
+  const [car, setCar] = useState({
+    name: 'ford',
+    model: 'mustang',
+    year: 2024,
+  });
   return (
     <div>
       <p>Car</p>
+      <p>
+        {car.name}
+        {car.model}
+        {car.year}
+      </p>
+      <input type="text" value={car.name}></input>
+      <br />
+      <br />
+      <input type="text" value={car.model}></input>
+      <br />
+      <br />
+      <input type="number"></input>
+      <br />
     </div>
   );
 }
+
+export default Components;
